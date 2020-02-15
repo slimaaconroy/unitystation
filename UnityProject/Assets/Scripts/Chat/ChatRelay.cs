cad557136594f77995132bc09bfcb8eba5946857
Doobly <rob@smashingpixels.com.au>
2020-02-15 18:29:10 +1000

    Adds message to chatrelay logs

--- UnityProject/Assets/Scripts/Chat/ChatRelay.cs
+++ UnityProject/Assets/Scripts/Chat/ChatRelay.cs
@@ -77,7 +77,7 @@ public class ChatRelay : NetworkBehaviour
 				if (players[i].Script == null)
 				{
 					//joined viewer, don't message them
-					Logger.Log($"1 Remove player {players[i].Name}");
+					Logger.Log($"1 Remove player {players[i].Name} : {chatEvent.message}");
 					players.Remove(players[i]);
 					continue;
 				}
@@ -85,35 +85,35 @@ public class ChatRelay : NetworkBehaviour
 				if (players[i].Script.IsGhost)
 				{
 					//send all to ghosts
-					Logger.Log($"2 Send to Ghost {players[i].Name}");
+					Logger.Log($"2 Send to Ghost {players[i].Name} : {chatEvent.message}");
 					continue;
 				}
 
 				if (chatEvent.position == TransformState.HiddenPos)
 				{
 					//show messages with no provided position to everyone
-					Logger.Log($"3 Chatevent is hidden pos send to: {players[i].Name}");
+					Logger.Log($"3 Chatevent is hidden pos send to: {players[i].Name} : {chatEvent.message}");
 					continue;
 				}
 
 				var dist = Vector2.Distance(chatEvent.position,
 						(Vector3)players[i].Script.WorldPos);
-				Logger.Log($"4 Dist from event to player: {dist} {players[i].Name}");
+				Logger.Log($"4 Dist from event to player: {dist} {players[i].Name} : {chatEvent.message}");
 				if (Vector2.Distance(chatEvent.position,
 						(Vector3)players[i].Script.WorldPos) > 14f)
 				{
-					Logger.Log($"5 Removing player as too far away {players[i].Name}");
+					Logger.Log($"5 Removing player as too far away {players[i].Name} : {chatEvent.message}");
 					//Player in the list is too far away for local chat, remove them:
 					players.Remove(players[i]);
 				}
 				else
 				{
-					Logger.Log($"6 {players[i].Name} is within range");
+					Logger.Log($"6 {players[i].Name} is within range : {chatEvent.message}");
 					//within range, but check if they are in another room or hiding behind a wall
 					if (Physics2D.Linecast(chatEvent.position,
 						(Vector3)players[i].Script.WorldPos, layerMask))
 					{
-						Logger.Log($"7 {players[i].Name} could not be reached by line cast. removing.");
+						Logger.Log($"7 {players[i].Name} could not be reached by line cast. removing. : {chatEvent.message}");
 						//if it hit a wall remove that player
 						players.Remove(players[i]);
 					}
@@ -145,10 +145,10 @@ public class ChatRelay : NetworkBehaviour
 				channels.HasFlag(ChatChannel.System) || channels.HasFlag(ChatChannel.Examine) ||
 				channels.HasFlag(ChatChannel.Action))
 			{
-				Logger.Log($"8 Attempt to send to player: {players[i].Name}");
+				Logger.Log($"8 Attempt to send to player: {players[i].Name} : {chatEvent.message}");
 				if (!channels.HasFlag(ChatChannel.Binary) || players[i].Script.IsGhost)
 				{
-					Logger.Log($"9 Sent to player {players[i].Name}");
+					Logger.Log($"9 Sent to player {players[i].Name} : {chatEvent.message}");
 					UpdateChatMessage.Send(players[i].GameObject, channels, chatEvent.modifiers, chatEvent.message, chatEvent.messageOthers,
 						chatEvent.originator, chatEvent.speaker);
 					continue;
@@ -167,7 +167,7 @@ public class ChatRelay : NetworkBehaviour
 			//if the mask ends up being a big fat 0 then don't do anything
 			if (channels != ChatChannel.None)
 			{
-				Logger.Log($"10 Players channel mask does not equal 0. Send it {players[i].Name}");
+				Logger.Log($"10 Players channel mask does not equal 0. Send it {players[i].Name} : {chatEvent.message}");
 				UpdateChatMessage.Send(players[i].GameObject, channels, chatEvent.modifiers, chatEvent.message, chatEvent.messageOthers,
 					chatEvent.originator, chatEvent.speaker);
 			}
